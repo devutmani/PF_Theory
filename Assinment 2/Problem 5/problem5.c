a3ddc8f808a1b47f0a75d3c594907d58bb769493
Dev Kumar <devutmani.2@gmail.com>
2024-12-01 17:02:30 +0500

    problem5.c

---
+++ Assinment 2/Problem 5/problem5.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main()
+{
+    int size , i , j;
+    printf("ENTER THE SIZE OF ARRAY:");
+    scanf("%d",&size);
+    int array[size];
+    for(i=0 ; i<size ; i++)
+    {
+        printf("ENTER THE VALUE THAT YOU WANT TO STORE ON COLUMN %d:",i+1) ;
+        scanf("%d",&array[i]);
+    }
+    printf("HORIZONTAL HISTOGRAM:\n");
+    for(i=0 ; i<size ; i++)
+    {
+        printf("Value %d: ",i+1);
+        for(j=0 ; j<array[i] ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("VERTICAL HISTOGRAM:\n");
+    int max = array[0];
+    for(i=0 ; i<size ; i++)
+    {
+        if(array[i]>max)
+        {
+            max = array[i];
+        }
+    }
+    int vertical_array[max][size];
+    for(i=max-1 ; i>=0 ; i--)
+    {
+        for(j=0 ; j<size ; j++)
+        {
+            if(array[j]>i)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }   
+        }
+        printf("\n");
+    }
+    for(i=0 ; i<size ; i++)
+    {
+        printf("%d ",array[i]) ;
+    }
+    return 0 ;
+}
